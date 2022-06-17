f12bb5f2babbb3c0da48277fc9e1a635e1c39707
Omar Alles <oalles@gmail.com>
2022-06-17 14:04:54 +0200

    on SET notify ONLINE

--- presence-platform/rg-keyspace-notifications-processor.py
+++ presence-platform/rg-keyspace-notifications-processor.py
@@ -8,3 +8,11 @@ gb.register(prefix='*',
             mode='sync',
             eventTypes=['expired'],
             readValue=False)
+
+gb = GB('KeysReader')
+gb.foreach(show)
+gb.foreach(lambda x: execute('XADD', 'presence', '*', 'status', 'ONLINE', 'client', x['key']))
+gb.register(prefix='*',
+            mode='sync',
+            eventTypes=['set'],
+            readValue=False)
